ac8c5bae22a38775ebafb6f286637e23416f1498
FatimaZelay <fatima.elay@gmail.com>
2025-01-16 16:21:56 +0100

    #15

---
+++ style.js
@@ -0,0 +1,15 @@
+{ 
+  
+    let interaction = document.querySelector('button')
+    
+   
+    interaction.addEventListener('click', Bouncing)
+    
+    function Bouncing() {
+    
+    
+    interaction.classList.toggle('bounceIn')
+    
+    }
+    
+    }
